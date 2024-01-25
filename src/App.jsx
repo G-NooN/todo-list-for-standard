d389e852397e9b17952da05dc7986464471a9c8d
GNooNJo <jjjchoon@nate.com>
2024-01-26 00:11:06 +0900

    refactor: separate TodoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import InputArea from "components/todo/InputArea";
+import TodoList from "components/todo/TodoList";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <InputArea todos={todos} setTodos={setTodos} />
+
+      {/* TodoList */}
+      <TodoList todos={todos} setTodos={setTodos} />
+
+      {/* Footer */}
+      <div></div>
     </div>
   );
 }
