02e12cba85225be51e66de9c32c7337517532746
GNooNJo <jjjchoon@nate.com>
2024-01-26 00:33:06 +0900

    refactor: separate Todo component

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,5 +1,54 @@
-function Todo() {
-  return <div>Todo</div>;
+function Todo({ todos, setTodos, checkDone }) {
+  return (
+    <div>
+      <h2>{!checkDone ? "Working" : "Done"}</h2>
+      <div>
+        {todos
+          .filter((todo) => todo.isDone === checkDone)
+          .map((todo) => {
+            return (
+              <div key={todo.id}>
+                <h3>{todo.title}</h3>
+                <p>{todo.contents}</p>
+                <button
+                  onClick={() => {
+                    const checkDelete = window.confirm("삭제하시겠습니까?");
+                    if (checkDelete) {
+                      const newTodos = todos.filter((todoItem) => todoItem.id !== todo.id);
+                      setTodos(newTodos);
+                      alert("삭제되었습니다.");
+                    } else return;
+                  }}
+                >
+                  삭제하기
+                </button>
+                <button
+                  onClick={() => {
+                    const checkEditDone = window.confirm("상태를 변경하시겠습니까?");
+                    if (checkEditDone) {
+                      const newTodos = todos.map((todoItem) => {
+                        if (todoItem.id === todo.id) {
+                          return {
+                            ...todoItem,
+                            isDone: !todoItem.isDone,
+                          };
+                        } else {
+                          return todoItem;
+                        }
+                      });
+                      setTodos(newTodos);
+                      alert("변경되었습니다.");
+                    }
+                  }}
+                >
+                  {!checkDone ? "완료" : "취소"}
+                </button>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,103 +1,10 @@
+import Todo from "./Todo";
+
 function TodoList({ todos, setTodos }) {
   return (
     <div>
-      {/* Working.. */}
-      <div>
-        <h2>Working</h2>
-        <div>
-          {todos
-            .filter((todo) => todo.isDone === false)
-            .map((todo) => {
-              return (
-                <div key={todo.id}>
-                  <h3>{todo.title}</h3>
-                  <p>{todo.contents}</p>
-                  <button
-                    onClick={() => {
-                      const checkDelete = window.confirm("삭제하시겠습니까?");
-                      if (checkDelete) {
-                        const newTodos = todos.filter((todoItem) => todoItem.id !== todo.id);
-                        setTodos(newTodos);
-                      } else return;
-                    }}
-                  >
-                    삭제하기
-                  </button>
-                  <button
-                    onClick={() => {
-                      const checkEditDone = window.confirm("상태를 변경하시겠습니까?");
-                      if (checkEditDone) {
-                        const newTodos = todos.map((todoItem) => {
-                          if (todoItem.id === todo.id) {
-                            return {
-                              ...todoItem,
-                              isDone: !todoItem.isDone,
-                            };
-                          } else {
-                            return todoItem;
-                          }
-                        });
-                        setTodos(newTodos);
-                        alert("변경되었습니다.");
-                      }
-                    }}
-                  >
-                    완료
-                  </button>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-
-      {/* Done!! */}
-      <div>
-        <h2>Done</h2>
-        <div>
-          {todos
-            .filter((todo) => todo.isDone === true)
-            .map((todo) => {
-              return (
-                <div key={todo.id}>
-                  <h3>{todo.title}</h3>
-                  <p>{todo.contents}</p>
-                  <button
-                    onClick={() => {
-                      const checkDelete = window.confirm("삭제하시겠습니까?");
-                      if (checkDelete) {
-                        const newTodos = todos.filter((todoItem) => todoItem.id !== todo.id);
-                        setTodos(newTodos);
-                      } else return;
-                    }}
-                  >
-                    삭제하기
-                  </button>
-                  <button
-                    onClick={() => {
-                      const checkEditDone = window.confirm("상태를 변경하시겠습니까?");
-                      if (checkEditDone) {
-                        const newTodos = todos.map((todoItem) => {
-                          if (todoItem.id === todo.id) {
-                            return {
-                              ...todoItem,
-                              isDone: !todoItem.isDone,
-                            };
-                          } else {
-                            return todoItem;
-                          }
-                        });
-                        setTodos(newTodos);
-                        alert("변경되었습니다.");
-                      }
-                    }}
-                  >
-                    취소
-                  </button>
-                </div>
-              );
-            })}
-        </div>
-      </div>
+      <Todo todos={todos} setTodos={setTodos} checkDone={false} />
+      <Todo todos={todos} setTodos={setTodos} checkDone={true} />
     </div>
   );
 }
